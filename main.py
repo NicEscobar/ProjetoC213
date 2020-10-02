d3141d5c0bd1eb8ba08b78f67fea5f6d2bf929cf
Nicole Escobar <nickyescobar_90@hotmail.com>
2020-10-02 17:29:31 -0300

    Implementando o PID

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 #Pegando as amostras de um  arquivo txt
 degrau0_2 = open('degrau0_2.txt', 'r')
-resp0_2 = open('resp0_2.txt', 'r')
+resp0_2 = open('resp0_2.txt', 'rb')
 tempo0_2 = open('tempo0_2.txt', 'r')
+ 
+a1 =  0.99557
+b1 =  0.0037426
 
-#Usando o metodo dos Minimos Quadrados
-[L, C] = size(resp0_2); #Pegando a linha (L) e coluna (C) da amostra de resposta
 
-if L > 1:
-    F = [resp0_2(1:L-1,1) degrau0_2(1:L-1,1)];
-    J = [resp0_2(2:L,1)];
+
+Kp = 4     #Ganho proporcional
+Ki = 0.1   #Ganho integral
+Kd = 1     #Ganho derivativo
+
+Ts = 0.1   #Tempo 
+
+SP = 50    #Setpoint
+PV = 0     #Precess Value
+
+resposta = 0 
+AcaoIntegral = 0
+
+ErroAnterior = SP - PV
+
+for i in np.arange(0.1, 100, 0.1): #o i vai de 0.1 até 100 passo 0.1
+
+  Erro = SP - PV
+
+  AcaoProporcional = Kp*Erro
+  AcaoIntegral = AcaoIntegral + Ki*Ts*Erro
+  AcaoDerivativa = ((Erro - ErroAnterior)/Ts)*Kd
+
+  AcaoControlador = AcaoProporcional + AcaoIntegral + AcaoDerivativa
+
+  PV = a1*PV + b1*AcaoControlador
+  resposta = [resposta, PV]
+  
+  #plt.plot(i, resposta)
+  #plt.show()
